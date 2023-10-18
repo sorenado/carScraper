549634c3d6093805896827e586a6eb271e24e701
Ron Shaked <ronshaked07@gmail.com>
2023-10-18 12:16:55 -0500

    Fixed Prettier Settings

--- server.js
+++ server.js
@@ -1,60 +1,60 @@
-const express = require("express");
-const { MongoClient, ObjectId } = require("mongodb");
-const crypto = require("crypto");
+const express = require('express');
+const { MongoClient, ObjectId } = require('mongodb');
+const crypto = require('crypto');
 const {
   sendConfirmation,
   hashStringToBase64,
-  checkUserAuthentication,
-} = require("./helperFunctions");
-const session = require("express-session");
-const MongoDBStore = require("connect-mongodb-session")(session);
+  checkUserAuthentication
+} = require('./helperFunctions');
+const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
 
 const app = express();
 const port = 8080;
 const dbURL =
-  "mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.0.1";
+  'mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.0.1';
 
 const store = new MongoDBStore({
   uri: dbURL, // MongoDB connection URL
-  collection: "sessions", // Collection to store sessions
+  collection: 'sessions' // Collection to store sessions
 });
 
 app.use(
   session({
-    secret: "your-secret-key",
+    secret: 'your-secret-key',
     resave: false,
     saveUninitialized: true,
     store: store,
     cookie: {
-      maxAge: 30 * 24 * 60 * 60 * 1000, // Session will last for 30 days
-    },
+      maxAge: 30 * 24 * 60 * 60 * 1000 // Session will last for 30 days
+    }
   })
 );
 async function connectToDB() {
   const client = new MongoClient(dbURL);
   try {
     await client.connect();
-    const db = client.db("car_scraper");
+    const db = client.db('car_scraper');
     return { db, client };
   } catch (err) {
     console.error(err);
-    console.log("Not connected to db");
+    console.log('Not connected to db');
   }
 }
 
 app.use(express.json());
-app.use(express.static("public"));
+app.use(express.static('public'));
 app.use(checkUserAuthentication);
 
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/homepage.html");
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/homepage.html');
 });
 
-app.post("/join-button-form", async (req, res) => {
+app.post('/join-button-form', async (req, res) => {
   try {
     const { db, client } = await connectToDB();
-    const pendingUserCollection = db.collection("pendingUsers");
-    const usersCollection = db.collection("users");
+    const pendingUserCollection = db.collection('pendingUsers');
+    const usersCollection = db.collection('users');
 
     const { name, email, password } = req.body;
     console.log(name, email, password);
@@ -62,7 +62,7 @@ app.post("/join-button-form", async (req, res) => {
     const user = await usersCollection.findOne({ email: email });
 
     if (user) {
-      console.log("User exists in the database:", user);
+      console.log('User exists in the database:', user);
       res.sendStatus(409); // If the user already has an account
       return; // Exit the function to prevent further execution
     }
@@ -73,12 +73,12 @@ app.post("/join-button-form", async (req, res) => {
     const pendingUser = {
       name: name,
       email: email,
-      password: hashedPassword,
+      password: hashedPassword
     };
 
     const result = await pendingUserCollection.insertOne(pendingUser);
     const inputtedPendingUser = await pendingUserCollection.find({
-      email: email,
+      email: email
     });
 
     if (inputtedPendingUser) {
@@ -95,49 +95,49 @@ app.post("/join-button-form", async (req, res) => {
   }
 });
 
-app.get("/addedUsersPage", async (req, res) => {
+app.get('/addedUsersPage', async (req, res) => {
   const objectId = req.query.objectid;
 
   try {
     const { db, client } = await connectToDB();
-    const userCollection = db.collection("users");
-    const pendingUserCollection = db.collection("pendingUsers");
+    const userCollection = db.collection('users');
+    const pendingUserCollection = db.collection('pendingUsers');
     const objectIdToQuery = new ObjectId(objectId);
 
     const pendingUser = await pendingUserCollection.findOne({
-      _id: objectIdToQuery,
+      _id: objectIdToQuery
     });
     const user = await userCollection.insertOne(pendingUser);
     const deletePendingUser = await pendingUserCollection.deleteOne({
-      _id: objectIdToQuery,
+      _id: objectIdToQuery
     });
 
     if (deletePendingUser.deletedCount === 1) {
-      res.status(200).sendFile(__dirname + "/addedUsersPage.html");
+      res.status(200).sendFile(__dirname + '/addedUsersPage.html');
       console.log(
-        "Everything worked correctly in making the user and adding to the database"
+        'Everything worked correctly in making the user and adding to the database'
       );
     } else {
-      res.status(500).sendFile(__dirname + "/addedUsersPage.html");
-      console.log("Deleted count was not 1 ");
+      res.status(500).sendFile(__dirname + '/addedUsersPage.html');
+      console.log('Deleted count was not 1 ');
     }
   } catch (err) {
     console.error(err);
-    res.status(500).sendFile(__dirname + "/addedUsersPage.html");
+    res.status(500).sendFile(__dirname + '/addedUsersPage.html');
   }
 });
 
-app.post("/login-form", async (req, res) => {
+app.post('/login-form', async (req, res) => {
   try {
     const { email, password } = req.body;
 
     const { db, client } = await connectToDB();
-    const userCollection = db.collection("users");
+    const userCollection = db.collection('users');
 
     const userInfo = await userCollection.findOne({ email: email });
 
     if (!userInfo) {
-      res.status(404).json({ message: "User not found" });
+      res.status(404).json({ message: 'User not found' });
       return;
     }
 
@@ -147,23 +147,23 @@ app.post("/login-form", async (req, res) => {
       // Create a session and store user information in the session
       req.session.user = {
         id: userInfo._id, // Assuming you have a unique user ID
-        email: userInfo.email,
+        email: userInfo.email
         // Add other user-related data as needed
       };
 
-      res.status(200).json({ message: "Login successful" });
-      console.log("Logged In");
+      res.status(200).json({ message: 'Login successful' });
+      console.log('Logged In');
     } else {
-      res.status(401).json({ message: "Incorrect password" });
+      res.status(401).json({ message: 'Incorrect password' });
     }
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: "Internal server error" });
+    res.status(500).json({ message: 'Internal server error' });
   }
 });
 
 // Add this route to your server.js
-app.get("/check-login-status", (req, res) => {
+app.get('/check-login-status', (req, res) => {
   // Check if the user is logged in based on the session
   const isLoggedIn = req.session.user ? true : false;
 
